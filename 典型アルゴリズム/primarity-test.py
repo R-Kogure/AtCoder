18f972df9ca4417088f0848d44dd48dd533a0d54
R-Kogure <78771040+R-Kogure@users.noreply.github.com>
2021-03-01 10:57:58 +0900

    Add files via upload

--- 典型アルゴリズム/primarity-test.py
+++ 典型アルゴリズム/primarity-test.py
@@ -2,6 +2,6 @@
 N = int(input())
 for i in range(2,int(N**0.5)+1):
     if N%i == 0:
-        print(str(N) + ' is not prime number')
+        print(f'{N} is not prime number')
         exit()
-print(str(N) + ' is prime number')
+print(f'{N} is prime number')
