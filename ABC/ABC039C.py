d19bc0df364dab6cfc9764f27874384ffce2a079
R-Kogure <78771040+R-Kogure@users.noreply.github.com>
2021-03-04 09:56:16 +0900

    Add files via upload

---
+++ ABC/ABC039C.py
@@ -0,0 +1,4 @@
+S = input()
+num = S.index('WBWBWBW')
+ans = {0:'Fa',1:'Mi',3:'Re',5:'Do',6:'Si',8:'La',10:'So'}
+print(ans[num])
---
+++ ABC/ABC043B.py
@@ -0,0 +1,9 @@
+#ABC043B
+s = input()
+ans = ''
+for i in s:
+    if i == 'B':
+        ans = ans[:-1]
+    else:
+        ans += i
+print(ans)
