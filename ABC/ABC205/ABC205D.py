b4ab066c84807aae08e3ec6a7282f7a2ffa79a50
R-Kogure <oujkog@gmail.com>
2021-06-16 07:41:07 +0900

    ACできた

--- ABC/ABC205/ABC205D.py
+++ ABC/ABC205/ABC205D.py
@@ -1,8 +1,32 @@
-#愚直なTLE解法
-N,Q = map(int,input().split())
-A = list(map(int,input().split()))
-tmp = list(i for i in range(10**18 + len(A)))
-diff = list(set(tmp) - set(A))
+import bisect
+N,Q = map(int, input().split()) 
+A = list(map(int, input().split()))
+
+dif=[]
+difs=[]
+difk={}
+tmp=0
+num=0
+for a in A:
+  if a-tmp>1:
+    num+=(a-tmp-1)
+    difs.append(num)
+    difk[num]=[a,tmp]
+  tmp=a
+if A[-1]!=10**20:
+  a=10**20
+  num+=(a-tmp-1)
+  difs.append(num)
+  difk[num]=[a,tmp]
+ 
 for i in range(Q):
-    k = int(input())
-    print(diff[k])
+  k = int(input())
+  index = bisect.bisect_left(difs, k)
+  if index!=0:
+    pre_index=index-1
+    a,b=difk[difs[index]]
+    c=difs[index-1]
+    print(k-c+b)
+  else:
+    a,b=difk[difs[index]]
+    print(k+b)
